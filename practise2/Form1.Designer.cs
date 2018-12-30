e7566d70a7aacd1ca908ad6cec65f79c1626d7c7
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-12-30 20:52:09 +0200

    Add files via upload

---
+++ practise2/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace practise2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(407, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(374, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "הגרל מספר";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ practise2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practise2
+{    
+    public partial class Form1 : Form
+    {
+        const int size = 10;
+        int i = 0;
+        int[] numbers = new int[size];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {          
+            
+            Random numrandom = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = numrandom.Next(0, 100);                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            label1.Text = Convert.ToString(numbers[i]);
+            if (i == size - 1)
+            {
+                i = 0;
+            }
+            else
+            {
+                i++;
+            }
+            
+        }
+    }
+}
